924335150bbe1cd2e9e759c3b80e5d7642080dd1
Aitana <aitanagp2@gmail.com>
2025-11-07 20:37:08 +0100

    Add users router and root endpoint to FastAPI app in main.py, and update AUTHORS

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+from app.routers import users
+
+app = FastAPI(debug=True)
+app.include_router(users.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my first FastAPI API"}
