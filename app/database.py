ad5ed6905b7d0ea4f74816bd3518d2902c9ebcd5
Aitana <aitanagp2@gmail.com>
2025-11-19 17:52:45 +0100

    added new file:   app/auth/auth.py and modified app/routers/users.py

---
+++ app/database.py
@@ -0,0 +1,4 @@
+from app.models import UserDb
+
+
+users: list[UserDb] = [UserDb(id=1, name="Alice", username="alice", password="alice")]
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,34 +1,18 @@
-from urllib import response
-from fastapi import APIRouter, status, HTTPException, Header
+import token
+from venv import create
+from app.auth.auth import create_access_token, Token, verify_password
+from app.models import UserIn, UserDb, UserOut, UserLoginIn
+from app.database import users
+
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, status, HTTPException, Header, Depends
 from pydantic import BaseModel
+
 
 router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
-
-class UserBase(BaseModel):
-    username: str
-    password: str
-
-class UserIn(UserBase):
-    name: str
-
-class UserDb(UserIn):
-    id: int
-
-class UserOut(BaseModel):
-    id: int
-    name: str
-    username: str
-
-class UserLoginIn(UserBase):
-    pass
-
-class TokenOut(BaseModel):
-    token: str
-    
-users: list[UserDb] = []
 
 @router.post("/signup/", status_code=status.HTTP_201_CREATED)
 async def create_user(userIn: UserIn):
@@ -48,31 +32,58 @@ async def create_user(userIn: UserIn):
         )
     )
 
-@router.post( "/login/",response_model=TokenOut, status_code=status.HTTP_200_OK)
-async def login(userLoginIn: UserLoginIn):
+@router.post( "/login/",
+             response_model=Token, 
+             status_code=status.HTTP_200_OK
+             )
+
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    #1. Busco username y passworrd en la peticion HTTP
+    username: str | None = form_data.get("username")
+    password: str | None = form_data.get("password")
+    
+    
+    if username is None or password is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username and/or password incorrect"
+        )
+    #2. Busco el usuario en la "base de datos"
     usersFound = [u for u in users if u.username == userLoginIn.username]
     if not usersFound:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Username and/or password incorrect"
         )
-    
+    #3. Verifico la contraseña
     user:UserDb = usersFound[0]
-    if user.password != userLoginIn.password:
+    if verify_password(password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Username and/or password incorrect"
         )
-    return TokenOut(
-        token=f"mytoken:{user.username}-{user.name}"
-    )
+        
+        token = create_access_token(
+            UserBase(
+                username=user.username, 
+                password=user.password
+            )
+        )
+        
+        return token
 
 @router.get("/", response_model=list[UserOut], status_code=status.HTTP_200_OK)
 async def get_all_users(authorization: str = Header()):
     print(authorization)
     
     parts = authorization.split(":")
-    if len(parts) != 2 or parts[0] != "mytoken":
+    if len(parts) != 2:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="forbidden"
+        )
+         
+    if parts[0] != "mytoken":
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="forbidden"
