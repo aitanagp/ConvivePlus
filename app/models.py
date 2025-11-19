7a05fec7c579c5ecd8938f525fb38e753565ca8e
Aitana Garcia Perez <agp@alu.edu.gva.es>
2025-11-19 17:52:45 +0100

    added new file:   app/auth/auth.py and modified app/routers/users.py

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+    password: str
+
+class UserIn(UserBase):
+    name: str
+
+class UserDb(UserIn):
+    id: int
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    username: str
+
+class UserLoginIn(UserBase):
+    pass
